83b1edd62cd882b8481704262ba34ea6f11f5834
SomeshBandal <someshbandal1312@gmail.com>
2025-12-05 13:37:01 +0530

    Update FamilyBackground fields to use Integer type
    Changed family member fields from String to Integer.

--- src/main/java/com/spring/jwt/entity/FamilyBackground.java
+++ src/main/java/com/spring/jwt/entity/FamilyBackground.java
@@ -32,16 +32,16 @@ public class FamilyBackground {
     private String motherOccupation;
 
     @Column(length = 45)
-    private String brother;
+    private Integer brother;
 
     @Column(length = 45)
-    private String marriedBrothers;
+    private Integer marriedBrothers;
 
     @Column(length = 45)
-    private String sisters;
+    private Integer sisters;
 
     @Column(length = 45)
-    private String marriedSisters;
+    private Integer marriedSisters;
 
     @Column(length = 45)
     private Boolean interCasteInFamily;
